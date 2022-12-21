c32bcc4b5b11bcb8fddf12b3ab1566ef35cdbe6b
cymula <amula@cytonn.com>
2022-12-21 22:50:35 +0300

    update-completed

--- crud-application/js/script.js
+++ crud-application/js/script.js
@@ -1,13 +1,23 @@
 "use strict";
 
+class Student {
+    constructor(student_id, student_name, student_email, student_dob) {
+        this.student_id = student_id;
+        this.student_name = student_name;
+        this.student_email = student_email;
+        this.student_dob = student_dob;
+    }
+}
+
+
 const array = [];
 const tHead = document.querySelector("thead");
 const tBody = document.querySelector("tbody");
 const table = document.querySelector("table");
+const form = document.querySelector("form");
 let rowIndex;
 
 const getStudents = () => {
-    console.log("Hii");
     const storage = JSON.parse(localStorage.getItem("student-data"));
     if (storage && storage.length >= 1) {
         console.log(storage);
@@ -61,19 +71,13 @@ const createStudent = document
         const student_email = document.getElementById("student_email").value;
         const student_dob = document.getElementById("student_dob").value;
 
+        const student = new Student(student_id, student_name, student_email, student_dob)
+
         if (student_name && student_id && student_email && student_dob) {
-            const formData = {
-                student_id,
-                student_name,
-                student_email,
-                student_dob,
-            };
-            array.push(formData);
-            console.log(array);
-            // save to local storage
-            saveToLocalStorage(array, formData);
+            array.push(student);
+            saveToLocalStorage(array, student);
+            form.reset()
         }
-        // add error class to form input
     });
 
 const saveToLocalStorage = (arr, new_Item) => {
@@ -105,7 +109,7 @@ table.onclick = () => {
     }
 };
 
-async function activateItem() {
+function activateItem() {
     rowIndex = this.rowIndex;
 
     let student_id = document.querySelector("#student_id");
@@ -113,10 +117,10 @@ async function activateItem() {
     let student_email = document.querySelector("#student_email");
     let student_dob = document.querySelector("#student_dob");
 
-    student_id.value = await this.cells[0].innerText;
-    student_name.value = await this.cells[1].innerText;
-    student_email.value = await this.cells[2].innerText;
-    student_dob.value = await this.cells[3].innerText;
+    student_id.value = this.cells[0].innerText;
+    student_name.value = this.cells[1].innerText;
+    student_email.value = this.cells[2].innerText;
+    student_dob.value = this.cells[3].innerText;
     console.log(rowIndex)
 };
 
@@ -128,5 +132,27 @@ const deleteStudent = () => {
         tableIndx && storage.splice(tableIndx, 1)
         localStorage.setItem("student-data", JSON.stringify(storage));
         createTableBody(storage)
+        form.reset()
     }
+}
+
+const updateStudent = () => {
+    console.log('hii')
+    const storage = JSON.parse(localStorage.getItem("student-data"))
+
+    let student_id = document.querySelector("#student_id").value;
+    let student_name = document.querySelector("#student_name").value;
+    let student_email = document.querySelector("#student_email").value;
+    let student_dob = document.querySelector("#student_dob").value;
+
+    const student = new Student(student_id, student_name, student_email, student_dob)
+
+    let tableIndx = rowIndex - 1;
+    if (tableIndx) {
+        tableIndx && storage.splice(tableIndx, 1, student)
+        localStorage.setItem("student-data", JSON.stringify(storage));
+        createTableBody(storage)
+        form.reset()
+    }
+
 }
